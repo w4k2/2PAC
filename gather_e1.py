03017a1428593dd8ddf081bb5e5c932a40d564bb
Joana <asiakom24@vp.pl>
2022-01-08 10:42:56 +0100

    fix e1_ddis + cleanup

--- gather_e1.py
+++ gather_e1.py
@@ -1,17 +1,18 @@
 import numpy as np
 
-mlp_res = np.load('res_e1_mlp.npy')
-mlp_errs = np.load('estim_err_mlp.npy')[:,:,:-1]
+mlp_res = np.load('results/res_e1_mlp.npy')
+mlp_errs = np.load('results/estim_err_mlp.npy')[:,:,:-1]
 
-sis_res = np.load('res_e1_sis.npy')
-sis_errs = np.load('estim_err_sis.npy')[:,:,:-4]
+sis_res = np.load('results/res_e1_sis.npy')
+sis_errs = np.load('results/estim_err_sis.npy')[:,:,:-4]
 
-# temp
-cdis_res = np.load('res_e1_sis.npy')
-cdis_errs = np.load('estim_err_sis.npy')[:,:,:-4]
-# temp
-ddis_res = np.load('res_e1_sis.npy')
-ddis_errs = np.load('estim_err_sis.npy')[:,:,:-4]
+cdis_res = np.load('results/res_e1_cdis.npy')
+cdis_errs = np.load('results/estim_err_cdis.npy')[:,:,:-4]
+
+ddis_res = np.load('results/res_e1_ddis.npy')
+ddis_errs = np.load('results/estim_err_ddis.npy')[:,:,:-4]
+
+print(ddis_res==cdis_res)
 
 
 # res
@@ -33,7 +34,7 @@ res_non_mlp_methods = np.concatenate((sis_methods, cdis_methods, ddis_methods), 
 res_all = np.concatenate((mlp_raw, res_non_mlp_raw, mlp_methods, res_non_mlp_methods), axis=2)
 print(res_all.shape)
 
-np.save('res_e1_all', res_all)
+np.save('results/res_e1_all', res_all)
 
 # errs
 
@@ -42,4 +43,4 @@ errs_all = np.concatenate((mlp_errs, errs_non_mlp), axis=2)
 
 print(errs_all.shape)
 
-np.save('errs_e1_all', errs_all)
+np.save('results/errs_e1_all', errs_all)
