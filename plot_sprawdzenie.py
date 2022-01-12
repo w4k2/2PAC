83293ba5173a870e8db6bab7b907790985bfd857
Joana <asiakom24@vp.pl>
2022-01-12 12:41:02 +0100

    nie rozumiem

--- plot_sprawdzenie.py
+++ plot_sprawdzenie.py
@@ -19,11 +19,17 @@ pe=3
 # exit()
 
 res = np.load('results/res_e1_all.npy')
+errs = np.mean(np.load('results/errs_e1_all.npy'),axis=0)
+
 raw_clfs_res = res[:,:,:len(base_clfs)]
 mean_raw_clfs_res = np.mean(raw_clfs_res, axis=0)[:,:,:,0] # (streams x clfs x chunks)
 
 meta_res = res[:,:,len(base_clfs):]
 meta_res = meta_res.reshape((reps,len(weights),len(base_clfs),len(criteria),len(borders),pe,chunks-1))
+
+errs = errs.reshape((len(weights), len(base_clfs), len(criteria), len(borders),pe,chunks-1))
+# print(errs.shape)
+# exit()
 # (reps x streams x base_clfs x criteria x  borders x (mean, prev, dsca) x chunks-1)
 
 mean_meta_res = np.mean(meta_res, axis=0)
@@ -40,7 +46,7 @@ for bc_id, bc in enumerate(base_clfs):
     sig = 5
 
     plt.clf()
-    fig, axx = plt.subplots(3,3,figsize=(8*1.618, 8),sharex=True, sharey=True)
+    fig, axx = plt.subplots(1,3,figsize=(12,5),sharex=True, sharey=True)
     fig.suptitle("%s" % (base_clfs_names[bc_id]), fontsize=14)
 
     axx = axx.ravel()
@@ -48,7 +54,8 @@ for bc_id, bc in enumerate(base_clfs):
     #raw clf res
     raw_res = mean_raw_clfs_res[:,bc_id]
 
-    for w_id, w in enumerate(weights):
+    for w_id, w in enumerate(config.str_weights_cdis()):
+
         ax = axx[w_id]
 
         ax.set_title(weigths_names[w_id])
@@ -56,24 +63,16 @@ for bc_id, bc in enumerate(base_clfs):
         ax.set_ylabel('BAC')
         ax.set_xlabel('chunk')
 
-        a = (raw_res[w_id].reshape(1,chunks-1,1))
-        b1 = (mean_mean[w_id,bc_id,0,5].reshape(1,chunks-1,1))
-        b2 = (mean_mean[w_id,bc_id,0,-1].reshape(1,chunks-1,1))
-        # c = scores_to_cummean(mean_dsca[w_id,bc_id,0,-1].reshape(1, chunks-1, 1))
-        # d = scores_to_cummean(mean_prev[w_id,bc_id,0,-1].reshape(1, chunks-1, 1))
-
-        # ax.plot(gaussian_filter1d(raw_res[w_id], sig), label='base clf', color='orange')
-        # ax.plot(gaussian_filter1d(mean_mean[w_id,bc_id,0,5],sig), ls='--', label='MEAN', c='tomato')
-        # ax.plot(gaussian_filter1d(mean_dsca[w_id,bc_id,0,-1],sig), ls='--', label='DSCA', c='dodgerblue')
-
+        a = (raw_res[3+w_id].reshape(1,chunks-1,1))
+        b1 = (mean_mean[3+w_id,bc_id,0,5].reshape(1,chunks-1,1))
+        b2 = (mean_mean[3+w_id,bc_id,0,-1].reshape(1,chunks-1,1))
+      
         ax.plot(a[0,:,0], label='base clf', color='orange')
         ax.plot(b1[0,:,0], ls='--', label='MEAN 0.3', c='tomato')
         ax.plot(b2[0,:,0], ls='--', label='MEAN 0.5', c='blue')
-        # ax.plot(c[0,:,0], ls='--', label='DSCA', c='dodgerblue')
-        # ax.plot(d[0,:,0], ls='--', label='PREV', c='forestgreen')
+        
+        ax.plot(errs[3+w_id, bc_id,0,0,0], label='err mean', c='purple')
 
-
-        ax.set_ylim(.5,1)
         ax.set_xlim(0, 500)
         ax.grid(ls=":")
         ax.spines['top'].set_visible(False)
