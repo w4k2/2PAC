7b205bafccda031a563288c6a490984a351d1560
TibetanSandFox <pawel.zyblewski@pwr.edu.pl>
2022-01-28 19:39:21 +0100

    t-test.

--- statistical1.py
+++ statistical1.py
@@ -4,11 +4,29 @@ from scipy.stats import wilcoxon, ttest_rel, ttest_ind
 from tabulate import tabulate
 import matplotlib.pyplot as plt
 from skimage.measure import label
-# import matplotlib.patches as patches
+from scipy import stats
+import matplotlib.patches as patches
 
 alpha = .05
 
+def t_test_corrected(a, b, J=1, k=10):
+    """
+    Corrected t-test for repeated cross-validation.
+    input, two 2d arrays. Repetitions x folds
+    As default for 5x5CV
+    """
+    if J*k != a.shape[0]:
+        raise Exception('%i scores received, but J=%i, k=%i (J*k=%i)' % (
+            a.shape[0], J, k, J*k
+        ))
 
+    d = a - b
+    bar_d = np.mean(d)
+    bar_sigma_2 = np.var(d.reshape(-1), ddof=1)
+    bar_sigma_2_mod = (1 / (J * k) + 1 / (k - 1)) * bar_sigma_2
+    t_stat = bar_d / np.sqrt(bar_sigma_2_mod)
+    pval = stats.t.sf(np.abs(t_stat), (k * J) - 1) * 2
+    return t_stat, pval
 
 """
 Statistical analysis
